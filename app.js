522c6e63ec132ca3312d5cf43767b56c6886953c
Rodrigo de Lima Cunha <rodrigo908@hotmail.com>
2018-10-23 08:40:40 -0300

    Atualizando

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var rotaNoticias = require('./app/routes/noticias')(app);
 var rotaFormInclusaonoticia = require('./app/routes/formulario_inclusao_noticias')(app);
 
 
-
 app.listen(3000, function(){
     console.log('Servidor rodando na porta 3000 com Express.');
 });
--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -4,7 +4,6 @@ var dbConnection = require('../../config/dbConnection');
 module.exports = function(app){
     app.get('/noticias', function(req, res){
 
-
         const connection = dbConnection();
           
           connection.query('select * from noticias', function(error, result){
