83479b45054785d0851bcad7ffdc1c3d2b20e42b
Rodrigo de Lima Cunha <rodrigo908@hotmail.com>
2018-10-26 08:01:42 -0300

    Atualizando validações com express-validator

--- app/models/NoticiasDAO.js
+++ app/models/NoticiasDAO.js
@@ -10,9 +10,9 @@ NoticiasDAO.prototype.getNoticia = function(callBack){
     this._connection.query('select * from noticias where id_noticia = 2', callBack);
 }
 
-NoticiasDAO.prototype.salvarNoticias = function(ArrayNoticia, callBack){
-    this._connection.query('insert into noticias (titulo, noticia) values ($1, $2)', 
-                     [ArrayNoticia.titulo, ArrayNoticia.noticia], 
+NoticiasDAO.prototype.salvarNoticias = function(ArrayDados, callBack){
+    this._connection.query('insert into noticias (titulo, noticia, resumo, autor, data_noticia) values ($1, $2, $3, $4, $5)', 
+                     [ArrayDados.titulo, ArrayDados.noticia, ArrayDados.resumo, ArrayDados.autor, ArrayDados.data_noticia], 
                      callBack);
 }
 
--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,11 +1,26 @@
 
 module.exports = function(application){
     application.get('/formulario_inclusao_noticias', function(req, resp){
-        resp.render("admin/form_add_noticia");
+        resp.render("admin/form_add_noticia", {validacao :{}, ArrayDados: {}});
     });
 
     application.post('/noticias/salvar', function(req, resp){
         var noticia = req.body;
+
+        req.assert('titulo','Título é obrigatório').notEmpty();
+        req.assert('resumo','Resumo é obrigatório').notEmpty();
+        req.assert('resumo','Resumo deve conter entre 10 e 100 caracteres').len(10, 100);
+        req.assert('autor','Autor é obrigatório').notEmpty();
+        //req.assert('data_noticia','Data é obrigatório').notEmpty().isDate({format:'YYYY-MM-DD'});
+        req.assert('data_noticia','Data é obrigatório').notEmpty();
+        req.assert('noticia','Notícia é obrigatório').notEmpty();
+
+        var erros = req.validationErrors();
+
+        if (erros){
+            resp.render("admin/form_add_noticia", {validacao : erros, ArrayDados: noticia});
+            return;
+        }
 
         const connection = application.config.dbConnection();
         var noticiasMD = new application.app.models.NoticiasDAO(connection);
